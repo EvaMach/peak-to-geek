87569a8ba8ab4c5d91d2f64e228244d865c9948b
Eva Machova <evca.machova@gmail.com>
2022-05-19 19:17:13 +0200

    přidat import routeru

--- client/Navigation/Navigation.jsx
+++ client/Navigation/Navigation.jsx
@@ -1,31 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <>
       <header>
-        <div id="logo" class="info__logo">
+        <div id="logo" className="info__logo">
           <Link to="/">
             <img src="img/logo-nerd-sloth.png" alt="Peak to Geek logo" />
           </Link>
         </div>
-        <nav id="navigation" class="info__navigation">
+        <nav id="navigation" className="info__navigation">
           <img id="hamburger" src="img/hamburger.svg" alt="Hamburger menu" />
           <ul id="navigation__items">
             <Link to="/info">
-              <li class="navigation__item navigation__item--actual">Info</li>
+              <li className="navigation__item navigation__item--actual">
+                Info
+              </li>
             </Link>
             <Link to="/dashboard">
-              <li class="navigation__item">Dashboard</li>
+              <li className="navigation__item">Dashboard</li>
             </Link>
             <Link to="/tree">
-              <li class="navigation__item">Tvůj Geek strom</li>
+              <li className="navigation__item">Tvůj Geek strom</li>
             </Link>
             <Link to="/courses">
-              <li class="navigation__item">Tvoje kurzy</li>
+              <li className="navigation__item">Tvoje kurzy</li>
             </Link>
             <Link to="/login">
-              <li class="navigation__item">
+              <li className="navigation__item">
                 <img src="img/icon__log-out.svg" alt="Ikona odhlášení" />
               </li>
             </Link>
