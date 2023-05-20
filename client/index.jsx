56b70e9b71f26a1858dc46c61fc4c4239d801481
Eva Machova <evca.machova@gmail.com>
2023-05-20 13:55:50 +0200

    add new structure

--- client/index.jsx
+++ client/index.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, useRoutes } from 'react-router-dom';
-import Home from './Home/Home.jsx';
-import Info from './Info/Info.jsx';
-import Login from './Login/Login.jsx';
-import Dashboard from './Dashboard/Dashboard.jsx';
-import Tree from './Tree/Tree.jsx';
-import Courses from './Courses/Courses.jsx';
-import Nelca from './Info/Nelca/Nelca.jsx';
-import Evca from './Info/Evca/Evca.jsx';
-import About from './Info/About/About.jsx';
+import HomePage from './pages/HomePage/HomePage';
+import InfoPage from './pages/InfoPage/InfoPage.jsx';
+import LoginPage from './pages/LoginPage/LoginPage.jsx';
+import DashboardPage from './pages/DashboardPage/DashboardPage.jsx';
+import TreePage from './pages/TreePage/TreePage.jsx';
+import CoursesPage from './pages/CoursesPage/CoursesPage.jsx';
+import Nelca from './pages/InfoPage/Nelca/Nelca.jsx';
+import Evca from './pages/InfoPage/Evca/Evca.jsx';
+import About from './pages/InfoPage/About/About.jsx';
 import './style.scss';
 import './DarkMode/DarkMode.scss';
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <HomePage />
     </div>
   );
 };
@@ -24,17 +24,17 @@ const App = () => {
 createRoot(document.querySelector('#app')).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="info" element={<Info />}>
+      <Route path="/" element={<HomePage />} />
+      <Route path="info" element={<InfoPage />}>
         <Route path="/info" element={<About />} />
         <Route exact path="/info/about" element={<About />} />
         <Route path="/info/nelca" element={<Nelca />} />
         <Route path="/info/evca" element={<Evca />} />
       </Route>
-      <Route path="login" element={<Login />} />
-      <Route path="dashboard" element={<Dashboard />} />
-      <Route path="tree" element={<Tree />} />
-      <Route path="courses" element={<Courses />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="dashboard" element={<DashboardPage />} />
+      <Route path="tree" element={<TreePage />} />
+      <Route path="courses" element={<CoursesPage />} />
     </Routes>
   </BrowserRouter>,
 );
